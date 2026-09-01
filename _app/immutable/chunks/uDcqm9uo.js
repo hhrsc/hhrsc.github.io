31921fa995be80961cdd476f0116c883653d7c14
your name <your email>
2026-09-01 22:46:25 +0800

    Serve website routes with real pages

---
+++ _app/immutable/chunks/uDcqm9uo.js
@@ -0,0 +1,1 @@
+import{c as m,a as i}from"./BbEN8DI_.js";import{f as n}from"./CYAcVWTh.js";import{s as p}from"./DkeGbvXj.js";function c(a,o){var r=m(),t=n(r);p(t,()=>o.children),i(a,r)}export{c as L};
